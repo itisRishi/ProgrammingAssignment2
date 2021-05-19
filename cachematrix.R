b84279744611c4b97bdf263ea518e77d23ee7350
itisRishi <sharmaritik478@gmail.com>
2021-05-19 15:35:52 +0530

    Updated R code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+##makeCacheMatrix function creates a list with methods to set and get a matrix and its inverse 
+## cacheSolve method takes list from the above function and attempts to find inverse in the cached data, if can't then it calculates the value and sets it for future use.
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## the makeCacheMatrix function will make a matrix mtx,
+##and has methods to set and get values for mtx and its Inverse
 
+makeCacheMatrix <- function(mtx = matrix()) {
+  z <- NULL
+     set <- function(y){
+       mtx <<- y
+       z <<- NULL
+     }
+     get <- function()mtx
+     setInverse <- function(inverse) z <<- inverse
+     getInverse <- function() z 
+     list(set = set, get = get, 
+          setInverse = setInverse, 
+          getInverse = getInverse)
+   
 }
+  
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+## cacheSolve method takes list from the makeCacheMatrix function and attempts to find inverse in the cached data if found the cached data is used else it calculates the value and sets it for future use.
+
+
+cacheSolve <- function(mtx, ...) {
+  z <- mtx$getInverse()
+     if(!is.null(z)){
+       message("getting cached data")
+       return(z)
+     }
+     mat <- mtx$get()
+     z <- solve(mat,...)
+     mtx$setInverse(z)
+     z
 }
